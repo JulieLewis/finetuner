4a6bde35d0aa5734d4719f1ebaadd19820e6e462
JulieLewis <juliebethlewis@gmail.com>
2024-12-11 20:24:36 -0500

    initial commit

---
+++ qlora_tuner.py
@@ -0,0 +1,153 @@
+# imports
+
+import os
+import re
+import math
+from tqdm import tqdm
+from huggingface_hub import login
+import torch
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, set_seed
+from peft import LoraConfig, PeftModel
+from datasets import load_dataset, Dataset, DatasetDict
+from datetime import datetime
+from dotenv import load_dotenv
+import wandb
+from trl import DataCollatorForCompletionOnlyLM
+
+
+BASE_MODEL = "Qwen/Qwen2.5-7B"
+DATASET_NAME = "ed-donner/pricer-data"
+MAX_SEQUENCE_LENGTH = 182
+
+RUN_NAME =  f"{datetime.now():%Y-%m-%d_%H.%M.%S}"
+PROJECT_RUN_NAME = f"pricer-{RUN_NAME}"
+HUB_MODEL_NAME = f"JulieLewis/{PROJECT_RUN_NAME}"
+# Hyperparameters for QLoRA
+
+LORA_R = 32
+LORA_ALPHA = 64
+TARGET_MODULES = ["q_proj", "v_proj", "k_proj", "o_proj"]
+LORA_DROPOUT = 0.1
+QUANT_4_BIT = True
+
+# Hyperparameters for Training
+
+EPOCHS = 3
+BATCH_SIZE = 16
+GRADIENT_ACCUMULATION_STEPS = 1
+LEARNING_RATE = 1e-4
+LR_SCHEDULER_TYPE = 'cosine'
+WARMUP_RATIO = 0.03
+OPTIMIZER = "paged_adamw_32bit"
+
+# Admin config
+
+STEPS = 50
+SAVE_STEPS = 3000
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_quant_type="nf4"
+  )
+dataset = load_dataset(DATASET_NAME)
+train = dataset['train']
+test = dataset['test']
+lora_parameters = LoraConfig(
+    lora_alpha=LORA_ALPHA,
+    lora_dropout=LORA_DROPOUT,
+    r=LORA_R,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=TARGET_MODULES,
+)
+
+# Next, specify the general configuration parameters for training
+
+train_parameters = SFTConfig(
+    output_dir=PROJECT_RUN_NAME,
+    num_train_epochs=EPOCHS,
+    per_device_train_batch_size=BATCH_SIZE,
+    per_device_eval_batch_size=1,
+    eval_strategy="no",
+    gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
+    optim=OPTIMIZER,
+    save_steps=SAVE_STEPS,
+    save_total_limit=10,
+    logging_steps=STEPS,
+    learning_rate=LEARNING_RATE,
+    weight_decay=0.001,
+    fp16=False,
+    bf16=True,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=WARMUP_RATIO,
+    group_by_length=True,
+    lr_scheduler_type=LR_SCHEDULER_TYPE,
+    report_to="wandb" if LOG_TO_WANDB else None,
+    run_name=RUN_NAME,
+    max_seq_length=MAX_SEQUENCE_LENGTH,
+    dataset_text_field="text",
+    save_strategy="steps",
+    hub_strategy="every_save",
+    push_to_hub=True,
+    hub_model_id=HUB_MODEL_NAME,
+    hub_private_repo=True
+)
+
+# And now, the Supervised Fine Tuning Trainer will carry out the fine-tuning
+# Given these 2 sets of configuration parameters
+response_template = "Price is $"
+collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+fine_tuning = SFTTrainer(
+    model=base_model,
+    train_dataset=train,
+    peft_config=lora_parameters,
+    tokenizer=tokenizer,
+    args=train_parameters,
+    data_collator=collator
+)
+
+def logins():
+    # Log in to HuggingFace
+    load_dotenv()
+
+    hf_token = os.getenv('HF_TOKEN')
+    login(hf_token, add_to_git_credential=True)
+    # Log in to Weights & Biases
+    wandb_api_key =  os.getenv('WANDB_API_KEY')
+    os.environ["WANDB_API_KEY"] = wandb_api_key
+    os.environ["WANDB_PROJECT"] = "qwen-pricer"
+    os.environ["WANDB_LOG_MODEL"] = "checkpoint"
+    os.environ["WANDB_WATCH"] = "gradients"
+
+    wandb.login()
+
+
+def load_base_model():
+    tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
+    base_model = AutoModelForCausalLM.from_pretrained(
+        BASE_MODEL,
+        quantization_config=quant_config,
+        device_map="auto",
+    )
+    base_model.generation_config.pad_token_id = tokenizer.pad_token_id
+
+def fine_tune():
+    # Fine-tune!
+    fine_tuning.train()
+
+    # Push our fine-tuned model to Hugging Face
+    fine_tuning.model.push_to_hub(PROJECT_RUN_NAME, private=True)
+    print(f"Saved to the hub: {PROJECT_RUN_NAME}")
+    wandb.finish()
+
+if __name__ == "__main__":
+    logins()
+    load_base_model()
+    fine_tune()
